027c115167466e9e4c855bfd71bc696c4234ee67
Innosa <innocentokafo@gmail.com>
2024-11-09 18:00:42 +0100

    all

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "skyblue",
+        color: "white",
+        textAlign: "center"
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo. I am proud of ALX.</p>
+    </main>
+  );
+}
+
+export default MainContent;
