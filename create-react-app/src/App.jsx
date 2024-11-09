d0d748f24a760978dd50def2778424711cdd64ac
Innosa <innocentokafo@gmail.com>
2024-11-09 12:16:14 +0100

    add header, maincontent and footer

--- create-react-app/src/App.jsx
+++ create-react-app/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import WelcomeMessage from ".components/WelcomeMessage.jsx";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+      <div>
+        <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +39,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>
+        <WelcomeMessage />
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
