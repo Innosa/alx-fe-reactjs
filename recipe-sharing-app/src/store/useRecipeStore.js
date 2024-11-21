b44296c686f4e9c76e806aea0623cf14188e83bb
Innosa <innocentokafo@gmail.com>
2024-11-21 06:41:38 +0100

    add components

--- recipe-sharing-app/src/store/useRecipeStore.js
+++ recipe-sharing-app/src/store/useRecipeStore.js
@@ -1,9 +1,13 @@
-import create from "zustand";
+import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
+  // Action to add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Action to initialize the recipe list
   setRecipes: (recipes) => set({ recipes })
 }));
 
