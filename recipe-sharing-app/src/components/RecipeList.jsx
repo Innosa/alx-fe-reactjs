d4d7fcefaa4a065facee0e11729d9a8d9fd1e2c6
Innosa <innocentokafo@gmail.com>
2024-11-23 13:17:13 +0100

    initial

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 // RecipeList component
-import useRecipeStore from "../components/recipeStore";
+import useRecipeStore from "./stores/recipeStore";
+
 import { Link } from "react-router-dom";
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -9,7 +10,7 @@ const RecipeList = () => {
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
-          {/* <p>{recipe.description}</p> */}
+          <p>{recipe.description}</p>
           <Link to={`/recipes/${recipe.id}`}>View Details</Link>
         </div>
       ))}
