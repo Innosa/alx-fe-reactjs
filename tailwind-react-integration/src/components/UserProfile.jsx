d391f791199a0d905075efca899eb780cf67eb18
Innosa <innocentokafo@gmail.com>
2024-12-01 17:09:07 +0100

    added new features

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="sm:p-4 md:p-8">
-      <div className="user-profile bg-gray-100  sm:p-4 .max-w-xs .max-w-sm mx-auto , my-20 rounded-lg shadow-lg ">
+    <div className="sm:p-4 md:p-8 md:max-w-sm mx-auto">
+      <div className="user-profile bg-gray-100  sm:p-4 .max-w-xs .max-w-sm mx-auto , my-20 rounded-lg shadow-lg hover:shadow-xl">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-36 .h-36 md:"
+          className="rounded-full w-24 h-24 sm:w-36 .h-36 md: hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="text-lg sm:text-xl md:text-2xl font-bold text-gray-800">
+        <h1 className="text-lg md:text-xl font-bold text-gray-800 hover:text-blue-500">
           John Doe
         </h1>
         <p className="text-sm sm:text-base">
