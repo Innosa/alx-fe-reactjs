3881b98323c3fef64728bdc3bfee216c336d4e66
Innosa <innocentokafo@gmail.com>
2024-12-01 16:09:53 +0100

    initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,20 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-8  max-w-xs sm:max-w-sm mx-auto , my-20 rounded-lg shadow-lg ">
-      <img
-        src="https://via.placeholder.com/150"
-        alt="User"
-        className="rounded-full w-24 h-24 sm:w-36 .h-36 md:"
-      />
-      <h1 className=" mt-4 text-lg sm:text-xl md:font-bold text-gray-800">
-        John Doe
-      </h1>
-      <p className="text-sm sm:text-base md:">
-        Developer at Example Co. Loves to write code and explore new
-        technologies.
-      </p>
+    <div className="p-4 sm:p-8 md:p-8">
+      <div className="user-profile bg-gray-100 max-w-xs sm:max-w-sm mx-auto , my-20 rounded-lg shadow-lg ">
+        <img
+          src="https://via.placeholder.com/150"
+          alt="User"
+          className="rounded-full w-24 h-24 sm:w-36 .h-36 md:"
+        />
+        <h1 className=" mt-4 text-lg sm:text-xl md:font-bold text-gray-800">
+          John Doe
+        </h1>
+        <p className="text-sm sm:text-base md:">
+          Developer at Example Co. Loves to write code and explore new
+          technologies.
+        </p>
+      </div>
     </div>
   );
 }
--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: { extend: {} },
