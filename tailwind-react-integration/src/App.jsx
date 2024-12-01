678b95990ed5f12cee1aa26e2ad0719644df8d26
Innosa <innocentokafo@gmail.com>
2024-12-01 14:58:28 +0100

    initial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import UserProfile from "./components/UserProfile.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <UserProfile />
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +29,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         This is to ensure my tailwindcss set up is working!
-      </p>
+      </p> */}
     </>
   );
 }
